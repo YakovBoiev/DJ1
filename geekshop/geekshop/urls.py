67c74edef3e6100882692156fd8675676f2959eb
Yakov Boiev <user.name>
2021-07-14 15:56:11 +0300

    create geekshop/views.py

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from geekshop.views import index, contacts
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/', include('mainapp.urls')),
+    path('', index),
+    path('contacts/', contacts)
 
 ]
