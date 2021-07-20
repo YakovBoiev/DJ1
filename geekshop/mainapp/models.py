a588e444e730b3d29f91b42504f522e6d4a11079
YakovBoiev <iakovboiev@gmail.com>
2021-07-20 22:30:34 +0200

    create admin

--- geekshop/mainapp/models.py
+++ geekshop/mainapp/models.py
@@ -1,14 +1,23 @@
 from django.db import models
 
+
 class ProductCategory(models.Model):
-    name = models.CharField(verbose_name='имя', max_length=64, unique=True)
-    description =  models.TextField(verbose_name='описание', blank=True)
+    name = models.CharField(
+        verbose_name='имя',
+        max_length=64,
+        unique=True, )
+    description = models.TextField(
+        verbose_name='описание',
+        blank=True, )
 
     def __str__(self):
         return self.name
 
+
 class Product(models.Model):
-    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    category = models.ForeignKey(
+        ProductCategory,
+        on_delete=models.CASCADE)
     name = models.CharField(verbose_name='имя продукта', max_length=128)
     image = models.ImageField(upload_to='products_images', blank=True)
     short_desc = models.CharField(verbose_name='краткое описание продукта', max_length=60, blank=True)
