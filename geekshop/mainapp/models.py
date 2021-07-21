a64b6a65e2482b4cce4e0bfc48ed8d0089141504
Yakov Boiev <user.name>
2021-07-21 16:15:57 +0300

    coding

--- geekshop/mainapp/models.py
+++ geekshop/mainapp/models.py
@@ -17,13 +17,28 @@ class ProductCategory(models.Model):
 class Product(models.Model):
     category = models.ForeignKey(
         ProductCategory,
+        verbose_name='категория',
         on_delete=models.CASCADE)
-    name = models.CharField(verbose_name='имя продукта', max_length=128)
-    image = models.ImageField(upload_to='products_images', blank=True)
-    short_desc = models.CharField(verbose_name='краткое описание продукта', max_length=60, blank=True)
-    descriptions = models.TextField(verbose_name='описание продукта', blank=True)
-    price = models.DecimalField(verbose_name='цена продукта', max_digits=8, decimal_places=2, default=0)
-    quanaty = models.PositiveIntegerField(verbose_name='количество на складе', default=0)
+    name = models.CharField(
+        verbose_name='имя продукта',
+        max_length=128)
+    image = models.ImageField(
+        upload_to='products_images',
+        blank=True)
+    short_desc = models.CharField(
+        verbose_name='краткое описание продукта',
+        max_length=60,
+        blank=True)
+    descriptions = models.TextField(
+        verbose_name='описание продукта',
+        blank=True)
+    price = models.DecimalField(
+        verbose_name='цена продукта',
+        max_digits=8,
+        decimal_places=2, default=0)
+    quanaty = models.PositiveIntegerField(
+        verbose_name='количество на складе',
+        default=0)
 
     def __str__(self):
         return f"{self.name} ({self.category.name})"
