bdf58f9a44f537f78b1faebdbcc3d25049f91d1f
Yakov Boiev <user.name>
2021-07-22 16:02:56 +0300

    finish coding less3

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,18 +1,27 @@
 from django.shortcuts import render
+from .models import ProductCategory, Product
 
 
 def index(request):
-    title = 'Каталог'
-    links_menu = [
-        {'href': 'products_all', 'name': 'все'},
-        {'href': 'product_home', 'name': 'дом'},
-        {'href': 'product_office', 'name': 'офис'},
-        {'href': 'product_modern', 'name': 'модерн'},
-        {'href': 'product_classic', 'name': 'классика'},
+    def create_links_menu():
+        links_menu = []
+        spam_dict = {'href': 'products_all', 'name': 'все'}
+        links_menu.append(spam_dict)
+        categories = ProductCategory.objects.all()
+        for category in categories:
+            spam_dict = {}
+            spam_dict['href'] = f'product_category_{category.id}'
+            spam_dict['name'] = category.name
+            links_menu.append(spam_dict)
+        return links_menu
 
-    ]
+    title = 'Каталог'
+    links_menu = create_links_menu()
+    products = Product.objects.all()[:3]
+
     context = {
         'title': title,
-        'links_menu': links_menu
+        'links_menu': links_menu,
+        'related_products': products
     }
     return render(request, 'mainapp/products.html', context)
