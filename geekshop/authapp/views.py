0939ef1ce6609c4a5c9a514cd74d6a949fa60985
Yakov Boiev <user.name>
2021-07-28 15:23:10 +0300

    create authapp/urls.py

--- geekshop/authapp/views.py
+++ geekshop/authapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def login(request):
+    title = 'Вход'
+    context = {
+        'title': title
+    }
+    return render(request, 'authapp/base.html', context)
+
+
+def logout(request):
+    pass
