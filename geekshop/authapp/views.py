a14718e634abac2e2965455b893db2fe41a934cd
YakovBoiev <iakovboiev@gmail.com>
2021-07-28 23:08:42 +0200

    coding

--- geekshop/authapp/views.py
+++ geekshop/authapp/views.py
@@ -1,13 +1,73 @@
+from django.contrib import auth
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+from .forms import ShopUserLoginForm, ShopUserEditForm, ShopUserRegisterForm
 
 
 def login(request):
     title = 'Вход'
+    login_form = ShopUserLoginForm(data=request.POST)
+    if request.method == 'POST' and login_form.is_valid():
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+        if user and user.is_active:
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('index'))
+
     context = {
-        'title': title
+        'title': title,
+        'login_form': login_form,
     }
-    return render(request, 'authapp/base.html', context)
+
+    return render(request, 'authapp/login.html', context)
 
 
 def logout(request):
-    pass
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
+
+
+def register(request):
+    title = 'Регистрация'
+
+    if request.method == 'POST':
+        register_form = ShopUserRegisterForm(request.POST, request.FILES)
+        if register_form.is_valid():
+            register_form.save()
+            return HttpResponseRedirect(reverse('auth:register'))
+
+    else:
+        register_form = ShopUserRegisterForm()
+
+    context = {
+        'title': title,
+        'register_form': register_form,
+    }
+
+    return render(request, 'authapp/register.html', context)
+
+
+def edit(request):
+    title = 'Профиль'
+
+    if request.method == 'POST':
+        edit_form = ShopUserEditForm(request.POST, request.FILES, instance=request.user)
+        if edit_form.is_valid():
+            edit_form.save()
+            return HttpResponseRedirect(reverse('auth:edit'))
+
+    else:
+        edit_form = ShopUserEditForm(instance=request.user)
+
+    context = {
+        'title': title,
+        'edit_form': edit_form,
+    }
+
+    return render(request, 'authapp/edit.html', context)
+
+
