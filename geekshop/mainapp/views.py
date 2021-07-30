3df6167b79cd2cc7a3b46438260beb13096fccf5
Yakov Boiev <user.name>
2021-07-30 13:54:58 +0300

    create less 5

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -2,21 +2,14 @@ from django.shortcuts import render
 from .models import ProductCategory, Product
 
 
-def index(request):
-    def create_links_menu():
-        links_menu = []
-        spam_dict = {'href': 'products_all', 'name': 'все'}
-        links_menu.append(spam_dict)
-        categories = ProductCategory.objects.all()
-        for category in categories:
-            spam_dict = {}
-            spam_dict['href'] = f'product_category_{category.id}'
-            spam_dict['name'] = category.name
-            links_menu.append(spam_dict)
-        return links_menu
+def index(request, pk=None):
 
-    title = 'Каталог'
-    links_menu = create_links_menu()
+    title = 'Продукты'
+    links_menu = ProductCategory.objects.all()
+
+
+
+
     products = Product.objects.all()[:3]
 
     context = {
