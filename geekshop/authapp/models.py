a95ad10b084d19fc2d8a6e7a03c78e1b9ad5c7c0
YakovBoiev <iakovboiev@gmail.com>
2021-08-18 20:16:06 +0200

    coding

--- geekshop/authapp/models.py
+++ geekshop/authapp/models.py
@@ -29,3 +29,38 @@ class ShopUser(AbstractUser):
     def is_activation_key_expires(self):
         return now() >= self.activation_key_expires
 
+class ShopUserprofile(models.Model):
+    MALE = 'M'
+    FEMALE = "Ж"
+
+    GENDER_CHOICES = (
+        (MALE, 'M'),
+        (FEMALE, 'Ж')
+    )
+
+    user = models.OneToOneField(
+        ShopUser,
+        unique=True,
+        null=False,
+        db_index=True,
+        on_delete=models.CASCADE
+    )
+
+    tagline = models.CharField(
+        verbose_name='тэги',
+        max_length=128,
+        blank=True,
+    )
+
+    about_me = models.TextField(
+        verbose_name='о себe',
+        max_length=512,
+        blank=True
+    )
+
+    gender = models.CharField(
+        verbose_name='',
+        max_length=1,
+        choices=GENDER_CHOICES,
+        blank=True,
+    )
