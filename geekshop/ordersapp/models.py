2dec38c5b153dca8980774d318fa847b9702179b
YakovBoiev <iakovboiev@gmail.com>
2021-08-24 15:00:54 +0200

    finish coding less3

--- geekshop/ordersapp/models.py
+++ geekshop/ordersapp/models.py
@@ -70,8 +70,10 @@ class Order(models.Model):
 
     def delete(self):
         for item in self.orderitems.select_related():
-            item.product.quantity += item.quantity
+            item.product.quanaty += item.quantity
             item.product.save()
+
+
         self.is_active = False
         self.save()
 
