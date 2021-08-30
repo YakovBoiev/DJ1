506d9dd39eaacc34c6456768b6d30b5a84a46050
Yakov Boiev <iakovboiev@gmail.com>
2021-08-30 06:48:33 +0300

    create less4

--- geekshop/ordersapp/views.py
+++ geekshop/ordersapp/views.py
@@ -5,7 +5,8 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, CreateView, UpdateView, DetailView, DeleteView
-
+from django.dispatch import receiver
+from django.db.models.signals import pre_save, pre_delete
 from .forms import OrderItemForm
 from .models import Order, OrderItem
 from basketapp.models import Basket
@@ -120,3 +121,21 @@ def order_forming_complete(request, pk):
     order.save()
 
     return HttpResponseRedirect(reverse('order:orders_list'))
+
+
+@receiver(pre_save, sender=OrderItem)
+@receiver(pre_save, sender=Basket)
+def product_quantity_update_save(sender, update_fields, instance, **kwargs):
+    if update_fields == 'quantity' or 'product':
+        if instance.pk:
+            instance.product.quanaty-= instance.quantity - sender.get_item(instance.pk).quantity
+        else:
+            instance.product.quanaty -= instance.quantity
+        instance.product.save()
+
+
+@receiver(pre_delete, sender=OrderItem)
+@receiver(pre_delete, sender=Basket)
+def product_quantity_update_delete(sender, instance, **kwargs):
+    instance.product.quanaty += instance.quantity
+    instance.product.save()
