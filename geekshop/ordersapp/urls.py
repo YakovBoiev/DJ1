c1c0c163188909ed46f75fc4de9c190cdc2ead14
Yakov Boiev <iakovboiev@gmail.com>
2021-09-02 13:22:55 +0300

    coding less5

--- geekshop/ordersapp/urls.py
+++ geekshop/ordersapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import OrderList, OrderItemsCreate, order_forming_complete, OrderRead, OrderUpdate, OrderDelete
+from .views import OrderList, OrderItemsCreate, order_forming_complete, OrderRead, OrderUpdate, OrderDelete, get_product_price
 
 app_name = 'ordersapp'
 
@@ -11,4 +11,5 @@ urlpatterns = [
     path('read/<int:pk>/', OrderRead.as_view(), name='order_read'),
     path('update/<int:pk>/', OrderUpdate.as_view(), name='order_update'),
     path('delete/<int:pk>/', OrderDelete.as_view(), name='order_delete'),
+    path('product/<int:pk>/price/', get_product_price, name='get-price'),
 ]
